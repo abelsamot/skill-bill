4bbe778e3e7de2ee80a036b85426ea1bb72b531d
abel samot <abelsamot@MacBook-Pro-de-abel.local>
2022-09-21 22:57:32 +0200

    first push

---
+++ test.py
@@ -0,0 +1,11 @@
+# Good luck mamen !
+import sys;
+
+def add(a,b):
+  return a+b
+
+if __name__ == "__main__":
+  y = int(sys.argv[1])
+  z = int(sys.argv[2])
+  result = int(sys.argv[3])
+  print(add(y,z)==result)
